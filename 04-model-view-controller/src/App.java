6fc9ccc765b697bb2c572508274bed47a81e0e4c
Yasmin Andrade <yasminrandrade014@gmail.com>
2026-08-11 14:10:22 -0300

    Add files via upload

---
+++ 04-model-view-controller/src/App.java
@@ -0,0 +1,9 @@
+import Controller.AlunoController;
+import Model.Aluno;
+import View.AlunoView;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
